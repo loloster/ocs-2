3adb053ff49a5ebfd84b241472565662562bbe39
loloster <loloster@gmail.com>
2018-04-25 12:05:38 +0200

    add nozosc.py

---
+++ nozosc.py
@@ -0,0 +1,314 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+# -*- mode: Python -*-
+
+# Le joystick bougé envoie automatiquement des messages knobs 30 et 31
+# Protocole sur le port série :
+
+# Au debut ca envoie rien.
+
+# FF arrête tous les envois en cours
+# A0 a AC  Les trucs qui oscillent 
+# 0 à 31 les knobs : 2 messages FF numeroknob 00 FF
+# N'importe quelle autre valeur va arrêter un 
+# F0 Donne le type de nozoid  "MMO3" : FF F0 4D 33
+# F1 ralenti
+# F2 accélère
+# 
+
+import sys
+from serial.tools import list_ports
+import serial,time
+from threading import Thread
+import gstt,socket
+import struct
+from OSC import OSCServer, OSCClient, OSCMessage
+import types
+
+oscIPin = socket.gethostbyname(socket.gethostname())
+oscPORTin = 8001
+oscpathin = ""
+
+oscIPout = ""
+oscPORTout = 8002
+
+oscdevice = 0
+
+NozMsg=[0,0,0,0]
+
+print("")
+print("OSCServer")
+print ("M controller is receiving on ", oscIPin, ":",str(oscPORTin))
+oscserver = OSCServer( (oscIPin, 8001) )
+oscserver.timeout = 0
+OSCRunning = True
+
+
+def handle_timeout(self):
+    self.timed_out = True
+
+
+
+def twoDigitHex( number ):
+   return '%02x' % number
+   
+def send(channel):
+    Mser.write([channel]) 
+    
+def XXXNozMsg(channel,value):
+    
+    print channel
+    print value
+    
+    
+#
+# OSC messages handlers
+#
+   
+oscserver.handle_timeout = types.MethodType(handle_timeout, oscserver)
+
+
+osclient = OSCClient()
+osclientme = OSCClient()
+oscmsg = OSCMessage()
+
+
+# sendosc(oscaddress, [arg1, arg2,...])
+def sendosc(oscaddress,oscargs):
+    
+    # also works : osclient.send(OSCMessage("/led", oscargs))
+
+    oscmsg = OSCMessage()
+    oscmsg.setAddress(oscaddress)
+    oscmsg.append(oscargs)
+    
+    print "sending : ",oscmsg
+    try:
+        osclient.sendto(oscmsg, (oscIPout, oscPORTout))
+        oscmsg.clearData()
+    except:
+        print ('Connection refused at ',oscIPout)
+        pass
+    #time.sleep(0.001)
+
+
+
+# sendme(oscaddress, [arg1, arg2,...])
+osclientme.connect((oscIPin, oscPORTin)) 
+
+def sendme(oscaddress,oscargs):
+        
+    oscmsg = OSCMessage()
+    oscmsg.setAddress(oscaddress)
+    oscmsg.append(oscargs)
+    
+    print "sending me: ",oscmsg
+    try:
+        osclientme.sendto(oscmsg, (oscIPin, oscPORTin))
+        oscmsg.clearData()
+    except:
+        print ('Connection to mycontroller refused')
+        pass
+    #time.sleep(0.001)
+  
+  
+  
+# RAW OSC Frame available ? 
+def osc_frame():
+    # clear timed_out flag
+    oscserver.timed_out = False
+    # handle all pending requests then return
+    while not oscserver.timed_out:
+        oscserver.handle_request()
+
+
+    
+#
+# OSC messages handlers
+#
+   
+# default handler 
+def handler(path, tags, args, source):
+
+	
+	oscpath = path.split("/")
+	pathlength = len(oscpath)
+	print ""
+	print "default handler"
+	print path, pathlength, oscpath, args
+
+	# /cc/number value
+	if oscpath[1] == "cc" :
+		number = int(oscpath[2])
+		value = int(args[0])
+		gstt.cc[number] = value    
+
+
+
+# /on
+def on(path, tags, args, source):
+    global oscIPout,oscdevice,controlmatrix
+
+    user = ''.join(path.split("/"))
+    #print "New OSC Client : " + str(source[0])
+    oscIPout = str(source[0])
+    osclient.connect((oscIPout, oscPORTout))
+    print ("Sending on ", oscIPout, ":",str(oscPORTout))
+    status("NozOSC ON")
+    print ("Stop Com from Nozoid")
+    Mser.write([0xFF])
+    print ("asking for with nozoid type...")
+    Mser.write([0xF0])
+
+# /stop
+def stop(path, tags, args, source):
+
+    print ("Stop Com from Nozoid")
+    Mser.write([0xFF]) 
+    time.sleep(1)
+    print "In_Waiting garbage msg # after 0xFF sent:",Mser.in_waiting
+    time.sleep(1)
+
+    while Mser.in_waiting != 0:
+        print "Still",Mser.in_waiting,"In_Waiting garbage msg after 0xFF sent"
+	Mser.read()
+    
+# /name 
+def name(path, tags, args, source):
+
+    print ("asking for with nozoid type...")
+    Mser.write([0xF0])
+    time.sleep(1)
+    print "In_Waiting garbage msg # after 0xF0 sent:",Mser.in_waiting
+    time.sleep(1)
+    for i in range(2):
+        print "!!"
+        print twoDigitHex(ord(Mser.read()[0])), twoDigitHex(ord(Mser.read()[0])), twoDigitHex(ord(Mser.read()[0])), twoDigitHex(ord(Mser.read()[0]))
+    print "In_Waiting garbage msg # after cleaning up try:",Mser.in_waiting
+    time.sleep(1)
+    
+    
+# /lfo
+def lfo(path, tags, args, source):
+	print ("LFO ", args[0], "asked")
+	Mser.write([0xA2 + int(args[0])]) # 0xA3 : LFO 1 / 0xA4 : LFO 2  / 0xA5 : LFO 3 
+
+
+# /osc
+def osc(path, tags, args, source):
+	print ("LFO ", args[0], "asked")
+	Mser.write([0x9F + int(args[0])]) # 0xA0 : OSC 1 / 0xA1 : OSC 2  / 0xA2 : OSC 3 
+
+# /down
+def down(path, tags, args, source):
+	print ("LFO ", args[0], "asked")
+	Mser.write([0x9F + int(args[0])]) # 0xA0 : OSC 1 / 0xA1 : OSC 2  / 0xA2 : OSC 3 
+
+# /up
+def up(path, tags, args, source):
+	print ("LFO ", args[0], "asked")
+	Mser.write([0x9F + int(args[0])]) # 0xA0 : OSC 1 / 0xA1 : OSC 2  / 0xA2 : OSC 3 
+
+
+
+# Send text to status display widget
+def status(text):
+    sendosc("/status", text)
+
+# registering all OSC message handlers
+
+oscserver.addMsgHandler( "/on", on )
+oscserver.addMsgHandler( "/stop", stop )
+oscserver.addMsgHandler("default", handler)
+oscserver.addMsgHandler( "/name", name )
+oscserver.addMsgHandler( "/lfo", lfo )
+oscserver.addMsgHandler( "/osc", osc )
+oscserver.addMsgHandler( "/up", up )
+oscserver.addMsgHandler( "/down", down )
+
+
+#
+# Running...
+#
+
+# TODO : Process events coming from Nozoid  in a separate thread. 
+def MSerialinProcess():
+
+    MESSAGE = 0xFF
+    
+    sermsg = [0,0,0,0,0,0]
+    
+    # pack bytes from serial port by 4. 
+    
+    while True:
+        sermsg[0] = ord(Mser.read())
+        if sermsg[0] == MESSAGE:
+
+            sermsg[1]= ord(Mser.read()[0])
+            sermsg[2]= twoDigitHex(ord(Mser.read()[0]))
+            sermsg[3]= twoDigitHex(ord(Mser.read()[0]))
+            sermsg[4]= twoDigitHex(ord(Mser.read()[0]))
+            sermsg[5]= twoDigitHex(ord(Mser.read()[0]))
+            
+            valhxx = "".join((str(sermsg[2]),str(sermsg[3]),str(sermsg[4]),str(sermsg[5])))
+            XXXNozMsg(sermsg[1],valhxx)
+            
+        time.sleep(0.02)
+        
+        
+# Search for nozoid
+
+print("")
+print("Available serial devices")
+ports = list(list_ports.comports())
+for p in ports:
+    print(p)
+
+
+try:
+
+	# Find serial port
+    sernozoid = next(list_ports.grep("sbmodemFA131"))
+    print "Serial Picked for Nozoid :",sernozoid[0]
+    Mser = serial.Serial(sernozoid[0],115200)
+    #Mser = serial.Serial(gstt.sernozoid[0],115200,timeout=5)
+    print "Serial connection..."
+    print "Device..." 
+    print(Mser.is_open)
+
+	# Serial port sync
+    print "In_Waiting garbage msg # at the serial opening:",Mser.in_waiting
+    while Mser.in_waiting != 0:
+        print "Still",Mser.in_waiting,"In_Waiting msg to flush at the opening"
+	Mser.read()
+
+	sendme("/stop")
+	sendme("/name")
+
+	# infinite loop display Nozoid message
+	# Todo transfer to a separate thread.
+    while True:
+	NozMsg = Mser.read(4)
+	osc_frame()
+	if ord(NozMsg[1]) < 160:
+		(val,) = struct.unpack_from('>H', NozMsg, 2)
+		print ''.join(("/nozoid/knob/",str(ord(NozMsg[1]))," ",NozMsg[0:2].encode('hex'), val))
+		sendosc(''.join(("/nozoid/knob/",str(ord(NozMsg[1]))," ",NozMsg[0:2].encode('hex'), val)))
+	else:  
+		(val,) = struct.unpack_from('>h', NozMsg, 2)
+		print ''.join(("/nozoid/oscitruc/",str(ord(NozMsg[1])-0x9F)," ",NozMsg[0:2].encode('hex'), val))
+		sendosc(''.join(("/nozoid/oscitruc/",str(ord(NozMsg[1])-0x9F)," ",NozMsg[0:2].encode('hex'), val)))
+    
+except StopIteration:
+    print ("No Nozoid device found")
+    Mser = False
+    
+if Mser != False:
+    pass
+ 
+
+
+
+
+
+
