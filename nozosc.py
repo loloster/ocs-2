9dd0795046d9406df43305dc0a2f1bf68d74f6d7
loloster <loloster@gmail.com>
2018-05-06 10:29:29 +0200

    add dmx and laser colorify code

--- nozosc.py
+++ nozosc.py
@@ -16,6 +16,8 @@
 # F2 accélère
 # 
 
+import random
+import pysimpledmx
 import sys
 from serial.tools import list_ports
 import serial,time
@@ -25,6 +27,31 @@ import struct
 from OSC import OSCServer, OSCClient, OSCMessage
 import types
 from sys import platform
+
+tLfoVal0 =  [0] * 256
+tLfoVal1 =  [0] * 256
+tLfoDelta = [0] * 256
+
+lfoval0=0
+lfoval1=0
+
+#DMX
+def senddmx0():
+    for channel in range (1,512):
+	senddmx(channel,0)
+
+def senddmx(channel, value):
+
+    if gstt.serdmx != "":
+        #mydmx.setChannel((channel + 1 ), value, autorender=True)
+        # calling render() is better more reliable to actually sending data
+
+        # Some strange bug. Need to add one to required dmx channel is done automatically
+        mydmx.setChannel((channel + 1 ), value)
+        mydmx.render()
+        print "Sending DMX Channel : ", str(channel), " value : ", str(value)
+
+
 
 #oscIPin = "192.168.42.194"
 #oscIPin = "127.0.0.1"
@@ -144,6 +171,13 @@ def sendosc(oscaddress,oscargs):
 	oscmsg.setAddress(oscaddress)
 	oscmsg.append(oscargs)
 
+    if oscpath[2] == "color":
+	print "we are asked to change lazer color"
+	oscmsg.setAddress(oscaddress)
+	if len(oscargs) > 0:
+		oscmsg.append(oscargs)
+
+
     #print "here we are sendosc function"
     #print "path:",oscaddress,"pathlength:", pathlength,"oscpath:", oscpath,"args:", oscargs
 
@@ -154,13 +188,19 @@ def sendosc(oscaddress,oscargs):
 
     #oscmsg.append(oscargs)
     
-    #print "sending : ",oscmsg
+
+    #print "oscmsg length:",len(oscmsg)
+    #print "sending:",oscmsg
+
+    #if len(oscmsg) > 0:
     try:
-        osclient.sendto(oscmsg, (oscIPout, oscPORTout))
-        oscmsg.clearData()
+	osclient.sendto(oscmsg, (oscIPout, oscPORTout))
+	oscmsg.clearData()
     except:
-        print ('Connection refused at ',oscIPout)
+	print ('Connection refused at ',oscIPout)
         pass
+    #else:
+#	print "Hum here in nozosc.py something went wrong with your %r msg" %oscpath[2]
     #time.sleep(0.001)
 
 
@@ -365,6 +405,40 @@ def nozY(path, tags, args, source):
 
 		gstt.Y=int(args[0])
 
+def nozcolor(path, tags, args, source):
+	print "Quelqu'un (je ne sais pas qui) m'a demandé de la couleur…"
+	print args
+	if 0 == len(args):
+		sendosc("/nozoid/color",[])
+		print "Hum maybe you should see now what bhorosc.py has answered about colorZ"
+		#gstt.color[0]=(gstt.colorX[0] or gstt.colorY[0])
+		#print "RX:%d RY:%d R:%d"%(gstt.colorX[0],gstt.colorY[0],gstt.color[0])
+		#gstt.color[1]=(gstt.colorX[1] or gstt.colorY[1])
+		#print "GX:%d GY:%d G:%d"%(gstt.colorX[1],gstt.colorY[1],gstt.color[1])
+		#gstt.color[2]=(gstt.colorX[2] or gstt.colorY[2])
+		#print "BX:%d BY:%d B:%d"%(gstt.colorX[2],gstt.colorY[2],gstt.color[2])
+
+	else:
+		print "Changing color to R:%d G:%d B:%d" % (args[0], args[1], args[2])
+		gstt.color[0]=int(args[0])
+		gstt.color[1]=int(args[1])
+		gstt.color[2]=int(args[2])
+		sendosc("/nozoid/color",[gstt.color[0],gstt.color[1],gstt.color[2]])
+
+def flashdmx(path, tags, args, source):
+
+	for channel in range (1,10):
+		vrand=random.randint(0,255)
+		senddmx(channel,vrand)
+
+	for channel in range (21,30):
+		vrand=random.randint(0,255)
+		senddmx(channel,vrand)
+
+	for channel in range (41,44):
+		vrand=random.randint(0,255)
+		senddmx(channel,vrand)
+
 # Send text to status display widget
 def nozstatus(path, tags, args, source):
     sendosc("/nozoid/status", args[0])
@@ -385,6 +459,8 @@ oscserver.addMsgHandler( "/nozoid/knob", nozknob )
 oscserver.addMsgHandler( "/nozoid/status", nozstatus )
 oscserver.addMsgHandler( "/nozoid/X", nozX )
 oscserver.addMsgHandler( "/nozoid/Y", nozY )
+oscserver.addMsgHandler( "/nozoid/color", nozcolor )
+oscserver.addMsgHandler( "/nozoid/flashdmx", flashdmx )
 
 
 #
@@ -400,13 +476,16 @@ ports = list(list_ports.comports())
 for p in ports:
     print(p)
 
+raw_input("Will try to select Last Serial Port\nPress Enter to continue...")
+
 try:
 
-    # Find serial port
+    # Find nozoid serial port
     if  platform == 'darwin':
         sernozoid = next(list_ports.grep("Arduino Due"))
     if  platform == 'linux2':
         sernozoid = next(list_ports.grep("ACM"))
+
 
     print "Serial Picked for Nozoid :",sernozoid[0]
     Mser = serial.Serial(sernozoid[0],115200)
@@ -431,10 +510,104 @@ try:
 
     #print ("asking for with nozoid type...")
     #the serial way please
-    #Mser.write([0xF0])
+    Mser.write([0xF0])
     #or the OSC way please !
     sendme("/nozoid/name","")
+
+except StopIteration:
+    print ("No Nozoid device found")
+    Mser = False
     
+if Mser != False:
+    pass
+
+    try:
+    # Find DMX serial port
+	if  platform == 'darwin':
+		gstt.serdmx = next(list_ports.grep("DMX USB PRO"))
+	if  platform == 'linux2':
+		gstt.serdmx = next(list_ports.grep("/dev/ttyUSB0"))
+
+	#print "gstt.serdmx", gstt.serdmx
+	#raw_input("Press Enter to continue...")
+
+	continueprint ("Serial Picked for DMX : ",gstt.serdmx[0])
+
+	if gstt.serdmx != "":
+		mydmx = pysimpledmx.DMXConnection(gstt.serdmx[0])
+
+	senddmx0()
+	time.sleep(1)
+
+	vrand=random.randint(0,255)
+	senddmx(1,vrand)#dimmer full
+	vrand=random.randint(0,255)
+	senddmx(3,vrand)#red
+	vrand=random.randint(0,255)
+	senddmx(4,vrand)#green
+	vrand=random.randint(0,255)
+	senddmx(5,vrand)#blue
+	vrand=random.randint(0,255)
+	senddmx(6,vrand)#
+	vrand=random.randint(0,255)
+	senddmx(7,vrand)#pan
+	vrand=random.randint(0,255)
+	senddmx(8,vrand)#change tilt to 180° (see http://static.boomtonedj.com/pdf/manual/43/43105_manuelfroggyledrgbw.pdf)
+	vrand=random.randint(0,255)
+	senddmx(9,vrand)#rotation speed
+
+	vrand=random.randint(0,255)
+	senddmx(21,vrand)
+	vrand=random.randint(0,255)
+	senddmx(22,0)
+	vrand=random.randint(0,255)
+	senddmx(23,vrand)
+	vrand=random.randint(0,255)
+	senddmx(24,0)
+	vrand=random.randint(0,255)
+	senddmx(25,0)
+	vrand=random.randint(0,255)
+	senddmx(26,255)
+	vrand=random.randint(0,255)
+	senddmx(27,255)
+	vrand=random.randint(0,255)
+	senddmx(28,255)
+	vrand=random.randint(0,255)
+	senddmx(29,255)
+	vrand=random.randint(0,255)
+#    senddmx(30,vrand)
+	vrand=random.randint(0,255)
+#    senddmx(31,vrand)
+	vrand=random.randint(0,255)
+#    senddmx(32,0)
+	vrand=random.randint(0,255)
+#    senddmx(33,vrand)
+
+	vrand=random.randint(0,255)
+	senddmx(41,vrand)
+	vrand=random.randint(0,255)
+	senddmx(42,vrand)
+	vrand=random.randint(0,255)
+	senddmx(43,vrand)
+
+	vrand=random.randint(0,255)
+#    senddmx(44,255)
+	vrand=random.randint(0,255)
+#    senddmx(45,255)
+	vrand=random.randint(0,255)
+#    senddmx(46,vrand)
+	vrand=random.randint(0,255)
+#    senddmx(47,vrand)
+	vrand=random.randint(0,255)
+
+    except StopIteration:
+	    print ("No DMX device found")
+	    mydmx = False
+    if mydmx != False:
+	pass
+#end DMX exception initialization
+#mydmx is *set* to false so can be checked for the following…
+
     while True:
 
         #print "loop"
@@ -450,11 +623,22 @@ try:
             sendosc("/nozoid/knob",''.join((twoDigit(ord(NozMsg[1])),str(val))))
         
          if ord(NozMsg[1]) >= 0xA0 and ord(NozMsg[1]) < 0xF0:
+
+	    OrdNozMsg=ord(NozMsg[1])
+
+	    tLfoVal0[OrdNozMsg]=tLfoVal1[OrdNozMsg]
+
             (val,) = struct.unpack_from('>h', NozMsg, 2)
 	    #print NozMsg
             #print type(NozMsg[0:2].encode('hex'))
             #print type(ord(val))
             #print '/nozoid/oscitruc'.join((str(ord(NozMsg[1])-0x9F)," ",NozMsg[0:2].encode('hex'),":",str(val)))
+
+	    tLfoVal1[OrdNozMsg]=val
+	    tLfoDelta[OrdNozMsg]=abs(tLfoVal1[OrdNozMsg]-tLfoVal0[OrdNozMsg])
+
+	    #print "delta lfo %x : %d" % (OrdNozMsg, tLfoDelta[OrdNozMsg])
+
             sendosc("/nozoid/osc",''.join((twoDigit(ord(NozMsg[1])-0x9F),str(val))))
 
          if ord(NozMsg[1]) == 0xF0:   
@@ -483,17 +667,12 @@ try:
             #sendosc("/nozoid/mix",''.join((twoDigit(ord(NozMsg[1])-0xF5),str(val))))
 
 
-
+'''
 except StopIteration:
-    print ("No Nozoid device found")
+    print ("No Nozoid or DMX device found")
+    print Mser
     Mser = False
     
 if Mser != False:
     pass
- 
-
-
-
-
-
-
+'''
